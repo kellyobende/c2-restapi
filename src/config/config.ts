4ae28f20a0c36a8637a430531971b5f970454355
obendekellyofuje <kellyobende@gmail.com>
2022-09-04 00:18:49 +0100

    third commit

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   "dev": {
     "username": "udagramobendedev",
-    "password": "Kelloggs1963$",
+    "password": "Kellogg1963$",
     "database": "udagramobendedev",
     "host": "udagramobendedev.cxfmjcyo5uic.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
@@ -20,3 +20,4 @@ export const config = {
     "dialect": "postgres"
   }
 }
+
