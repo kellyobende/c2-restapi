c310007edd20f7bcb5fb8fdb305703fe0a52f6a4
obendekellyofuje <kellyobende@gmail.com>
2022-09-03 11:37:11 +0100

    New changes made

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "udagramobendedev",
+    "password": "Kelloggs1963$",
+    "database": "udagramobendedev",
+    "host": "udagramobendedev.cxfmjcyo5uic.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-obende-dev"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
