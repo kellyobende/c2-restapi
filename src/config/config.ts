a43a3b78cd836b07aa89a9347604866829a331e7
obendekellyofuje <kellyobende@gmail.com>
2022-09-04 16:21:15 +0100

    more adjustments on the env variable performed

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": process.env.POSTGRESS_USERNAME,
-    "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DATABASE,
-    "host": process.env.POSTGRESS_HOST,
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": process.env.POSTGRES_DIALECT,
-    "aws_region": process.env.POSTGRESS_AWS_REGION,
-    "aws_profile": process.env.POSTGRESS_AWS_PROFILE,
-    "aws_media_bucket": process.env.POSTGRESS_AWS_MEDIA_BUCKET
+    "aws_region": process.env.POSTGRES_AWS_REGION,
+    "aws_profile": process.env.POSTGRES_AWS_PROFILE,
+    "aws_media_bucket": process.env.POSTGRES_AWS_MEDIA_BUCKET
   },
   "jwt": {
     "secret": " "
